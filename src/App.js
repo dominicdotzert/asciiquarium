e0291a59bebc086d5e5c80d570ec9a623c03cc6a
syuyitung <samyuyitung@gmail.com>
2017-02-04 16:38:38 -0500

    added render array function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: [[]]
+      rendered: {}
+    };
+  }
+
+  renderArray() {
+    var str = "";
+    this.state.array.forEach(function(row){
+      str += "<p>";
+      row.forEach(function(char){
+        str += char;
+      });
+      str += "</p>";
+    });
+    return str;
+  }
+
   render() {
-    return (
-      <div className="App">
-        <p>
-        --------------------------------------------------------------------------------------------------------------------------------------------------<br />
-        ~               ~          ~ ~ ~                ~             ~ ~ ~ 
-        </p>
-      </div>
+    return (  
+      <div className="App" dangerouslySetInnerHTML={{ __html: this.renderArray() }} />
     );
   }
 }
